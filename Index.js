59a30efd127fafb2c796fef135caf69fc8041d65
WoldegebrielKebrom <kebrom_woldegebriel@mckinsey.com>
2022-05-13 14:43:23 -0400

    Associations and Associations test

--- Index.js
+++ Index.js
@@ -2,6 +2,12 @@ const {User} = require('./User');
  const {Board} = require('./Board');
  const {Cheese} =  require('./Cheese');
 
+Board.belongsTo(User);
+User.hasMany(Board);
+
+
+Board.belongsToMany(Cheese, {through: "board_cheese"});
+Cheese.belongsToMany(Board, {through: "board_cheese"});
 
 
 module.exports = {
--- index.test.js
+++ index.test.js
@@ -48,7 +48,59 @@ describe('Board,Cheese, and User Model', () => {
         })
 
         expect(newCheese.title).toBe('American');
+    });
+
+
+    test('test one-to-many association', async () => {
+     
+        const user = await User.findByPk(1);
+        await user.addBoard(1);
+        const  board = await Board.findAll();
+         expect(board[0].dataValues.userId).toBe(1);
+           
+    });
+
+
+    test('test many-to-many association', async () => {
+        // TODO - test creating a song
+        
+        
+
+        const board = await Board.findByPk(1);
+        await board.addCheese(1);
+
+        const cheese = await Cheese.findByPk(1);
+        await cheese.addBoard(1);
+
+        
+        const boards = await Board.findAll({
+            include : [Cheese]
+        })
+
+         //console.log('something',boards[0].dataValues.cheeses[0].dataValues.title);
+
+        expect(boards[0].dataValues.cheeses[0].dataValues.title).toBe('American');
+    });
+
+    test('test eager load', async () => {
+        // TODO - test creating a musician
+        const cheese = await Cheese.findAll({
+            include : [{
+                model:Board, as: 'boards'
+            }]
+        });
+
+       
+        //console.log("edger loading", cheese[0].dataValues.boards[0].dataValues.type);
+        
+
+
+        
+        expect(cheese[0].dataValues.boards[0].dataValues.type).toBe('board1 type');
+       
+
     })
+
 
     
 
